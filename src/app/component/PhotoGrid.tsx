47b3fa80db2d2ca9ec09c2fbc09dfaf939801925
Boonyarith P <boonyarith.pir@gmail.com>
2024-05-28 00:20:08 +0700

    photo pagination

--- src/app/component/PhotoGrid.tsx
+++ src/app/component/PhotoGrid.tsx
@@ -1,25 +1,52 @@
 "use client";
+import { LoadingSpinner } from "@/app/component/LoadingSpinner";
 import { PhotoItem } from "@/app/component/PhotoItem";
 import { PhotoModal } from "@/app/component/PhotoModal";
-import { PhotoInfo, getAllPhotos } from "@/service/gallery/photos";
-import { useEffect, useState } from "react";
+import { PhotoInfo, getPhotoListWithPagination } from "@/service/gallery/photos";
+import { useEffect, useRef, useState } from "react";
+
+let reachedTheEnd = false;
 
 export const PhotoGrid = () => {
   const [photos, setPhotos] = useState<PhotoInfo[]>([]);
   const [selectedPhoto, setSelectedPhoto] = useState<PhotoInfo | null>(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const getPhotos = async () => {
-    const photoRecords = await getAllPhotos();
-    setPhotos(photoRecords);
+    if (reachedTheEnd) return;
+
+    setLoading(true);
+    const newPhotos = await getPhotoListWithPagination(page, 18);
+    reachedTheEnd = newPhotos.length === 0;
+    setPhotos((prevPhotos) => {
+      const mergedPhotos = new Map();
+      prevPhotos.forEach((photo) => mergedPhotos.set(photo.id, photo));
+      newPhotos.forEach((photo) => mergedPhotos.set(photo.id, photo));
+      return Array.from(mergedPhotos.values());
+    });
+    setPage(page + 1);
+    setLoading(false);
   };
 
   useEffect(() => {
-    getPhotos();
-  }, []);
+    const loadMore = () => {
+      if (containerRef.current && containerRef.current.getBoundingClientRect().bottom <= window.innerHeight) {
+        getPhotos();
+      }
+    };
+
+    loadMore();
+    window.addEventListener("scroll", loadMore);
+    return () => {
+      window.removeEventListener("scroll", loadMore);
+    };
+  }, [page]);
 
   return (
-    <div>
-      <div className="grid grid-cols-3 gap-1 w-full mx-auto">
+    <div ref={containerRef}>
+      <div className="grid grid-cols-3 gap-1 w-full mx-auto pb-8">
         {photos.map((photo, i) => (
           <PhotoItem
             key={photo.id}
@@ -30,6 +57,11 @@ export const PhotoGrid = () => {
         ))}
       </div>
       {!!selectedPhoto && <PhotoModal photo={selectedPhoto} close={() => setSelectedPhoto(null)} />}
+      {loading && (
+        <div className="w-full flex flex-col items-center py-4">
+          <LoadingSpinner />
+        </div>
+      )}
     </div>
   );
 };
--- src/service/gallery/photos.ts
+++ src/service/gallery/photos.ts
@@ -52,6 +52,45 @@ export const getAllPhotos = async (): Promise<PhotoInfo[]> => {
   });
 };
 
+export const getPhotoListWithPagination = async (page: number, perPage: number): Promise<PhotoInfo[]> => {
+  const result = await prisma.photo.findMany({
+    orderBy: {
+      id: "desc",
+    },
+    include: {
+      vibeSong: true,
+    },
+    skip: (page - 1) * perPage,
+    take: perPage,
+  });
+
+  return result.map((photo) => {
+    return {
+      id: photo.id.toString(),
+      photoUrl: `${process.env.MINIO_PHOTO_PATH}/${photo.fileName}`,
+      createdAt: photo.createdAt?.toISOString() || "",
+      exif: {
+        iso: photo.iso,
+        shutterSpeed: photo.shutterSpeed,
+        fNumber: photo.fNumber,
+        focalLength: photo.focalLength,
+        maker: photo.maker,
+        model: photo.model,
+        lensModel: photo.lensModel,
+        createDate: photo.createDate?.toISOString() || "",
+        imageHeight: photo.imageHeight,
+        imageWidth: photo.imageWidth,
+      },
+      story: photo.story || "",
+      vibeSong: {
+        youtubeId: photo.vibeSong?.youtubeId ?? undefined,
+        startTime: photo.vibeSong?.startTime?.toFixed() ?? undefined,
+        endTime: photo.vibeSong?.endTime?.toFixed() ?? undefined,
+      },
+    };
+  });
+}
+
 export const getTotalNumberOfPhotos = async (): Promise<number> => {
   return await prisma.photo.count();
 };
